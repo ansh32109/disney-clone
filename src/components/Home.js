ff7c0cf4081bd3c08567c53208cf53ca29d85dea
Ainesh Sinha <studentainesh@gmail.com>
2023-12-31 21:21:17 +0530

    Added recommended section

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,20 @@ import styled from "styled-components";
 import homebg from '../assets/images/home-background.png';
 import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
+import Recommends from "./Recommends";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
+import Trending from "./Trending";
 
 const Home = (props) => {
     return (
         <Container>
             <ImgSlider/>
             <Viewers />
+            <Recommends/>
+            <NewDisney/>
+            <Originals/>
+            <Trending/>
         </Container>
     )
 };
